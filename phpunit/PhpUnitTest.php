071b655592cf9824c5162f1508a9e9f286d2e574
Maxim Starikov <mstarikov@elance-odesk.com>
2020-07-24 00:18:25 +0200

    add unit test

---
+++ phpunit/PhpUnitTest.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class PhpUnitTest extends TestCase
+{
+    public function testTriialThing(): void
+    {
+        $this->assertTrue(
+            true
+        );
+    }
+}
